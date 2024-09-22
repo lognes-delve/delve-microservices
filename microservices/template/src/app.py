f81057b863b82fa15ca5e8e19bc85f6584cdc943
l0gnes <loonargt@gmail.com>
2024-09-21 23:00:21 -0300

    Basic infrastructure setup(?)

---
+++ microservices/template/src/app.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from starlette.responses import JSONResponse
+
+app = FastAPI()
+
+@app.get("/")
+async def test_endpoint() -> JSONResponse:
+    return JSONResponse(
+        {"ok" : True}
+    )
+
