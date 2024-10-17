a142340dbd57725c30c9358e7b6fa3dc9278f1a7
l0gnes <loonargt@gmail.com>
2024-10-17 16:03:46 -0300

    add database ping to test route

--- microservices/template/src/app.py
+++ microservices/template/src/app.py
@@ -1,11 +1,20 @@
 from fastapi import FastAPI
 from starlette.responses import JSONResponse
 
+from delve_common._db._database import Database, get_client
+
 app = FastAPI()
+
+Database.using_app(app)
 
 @app.get("/")
 async def test_endpoint() -> JSONResponse:
+
+    db = await get_client() 
+
+    db_resp = await db.admin.command({"ping" : 1})
+
     return JSONResponse(
-        {"ok" : "True"}
+        {"ok" : "True", "db" : db_resp}
     )
 
